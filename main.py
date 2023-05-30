2481bfdd330541490a667e36bfbdbb6caf6a35ab
Julia Barbosa <juliadealencarb@gmail.com>
2023-05-30 11:31:23 +0100

    Adding list posts, see specific post and create new post funcionalities.

--- main.py
+++ main.py
@@ -5,25 +5,72 @@ import calendar
 import smtplib
 import os
 from dotenv import load_dotenv
+from flask_ckeditor import CKEditor, CKEditorField
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
 
 load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.secret_key = "secret"
+
+db = SQLAlchemy(app)
+ckeditor = CKEditor(app)
+Bootstrap(app)
 
 
-response = requests.get("https://api.npoint.io/56bcff06a50803bc404b")
-data = response.json()
+class BlogPost(db.Model):
+    # id, title, subtitle, date, body, author, img_url
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), unique=True, nullable=False)
+    subtitle = db.Column(db.String(255), nullable=False)
+    date = db.Column(db.String(100), nullable=True, default=datetime.datetime.utcnow)
+    body = db.Column(db.String(500), nullable=False)
+    author = db.Column(db.String(255), nullable=False)
+    img_url = db.Column(db.String(2083), nullable=False)
 
-today = datetime.date.today()
-day = today.day
-month = calendar.month_name[today.month]
-year = today.year
-formatted_date = f"{month} {day}, {year}"
+
+# with app.app_context():
+#     db.create_all()
+
+
+class PostForm(FlaskForm):
+    # title, subtitle, author, img_url, body, submit
+    title = StringField("Post title", validators=[DataRequired()])
+    subtitle = StringField("Post subtitle", validators=[DataRequired()])
+    author = StringField("Author name", validators=[DataRequired()])
+    img_url = StringField("Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Post text", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+class BlogService:
+    @staticmethod
+    def get_current_date():
+        today = datetime.date.today()
+        day = today.day
+        month = calendar.month_name[today.month]
+        year = today.year
+        formatted_date = f"{month} {day}, {year}"
+
+        return formatted_date
+
+    @staticmethod
+    def get_posts():
+        posts = db.session.query(BlogPost).all()
+        return posts
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", data=data, date=formatted_date)
+    return render_template("index.html", all_posts=BlogService.get_posts(), date=BlogService.get_current_date())
 
 
 @app.route("/about")
@@ -53,11 +100,36 @@ def contact():
         return render_template("contact.html", sent=False)
 
 
+@app.route("/posts", methods=['POST', 'GET'])
+def create_new_post():
+    form = PostForm()
+
+    if request.method == 'POST':
+        data = request.form.get('body')
+
+        if form.validate_on_submit():
+            new_post = BlogPost(title=form.title.data,
+                                subtitle=form.subtitle.data,
+                                body=data,
+                                author=form.author.data,
+                                img_url=form.img_url.data)
+            db.session.add(new_post)
+            db.session.commit()
+
+    return render_template("make-post.html", form=form)
+
+
 @app.route("/posts/<int:post_id>")
 def load_post(post_id):
-    for post in data:
-        if post["id"] == post_id:
-            return render_template("post.html", post=post, date=formatted_date)
+    posts = BlogService.get_posts()
+    for post in posts:
+        if post.id == post_id:
+            return render_template("post.html", post=post, date=BlogService.get_current_date())
+
+
+@app.route("/posts/<int:post_id>", methods=['PATCH'])
+def edit_post(post_id):
+    pass
 
 
 if __name__ == "__main__":
